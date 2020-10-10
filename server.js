b6af48889e36db66148dcdd41b5931df891248e6
wilmerbaby <wilmerbaby@gmail.com>
2020-10-10 12:22:07 -0400

    started server on  PORT 8080 added packages and server file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+const app = express();
+const PORT = precess.env.PORT || 80080;
+
+app.get("/api/config",(req, res) => {
+    res.json({
+        success:true,
+    });
+});
+
+app.listen(PORT,() =>{
+    console.log(`server is runinin on http://localhost:${PORT}`)
+}
+)
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "morter706",
+  database: "",
+});
