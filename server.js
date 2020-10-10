245332a99e90455bbdf17b5fe8c8a64d5ac2b8e1
wilmerbaby <wilmerbaby@gmail.com>
2020-10-10 14:54:42 -0400

    injected value in server.js file to test resnder into the htm.handlebars to then show on herokuroposotry site

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const exphbs = require("express-handlebars");
 const mysql = require("mysql");
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+//app middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//midleware for handelbars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//get rout to render the whats in the index file in main.handlbars file also able to inject values when you res.render
+//rendering into index to show on main handlebars file
 app.get("/",(req, res) =>{
-    res.render("index");
+    res.render("index", {name: "Wilmer Rivera Morter"});
 });
 // test rout/get rout to a web browser api page 
 app.get("/api/config",(req, res) => {
